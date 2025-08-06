37f8b311cb7acdd058dfc5765b49b9cdb21a3e73
tamaro <tamarowalter@yahoo.de>
2025-08-06 12:07:07 +0200

    add function for effects

--- json_controller.cpp
+++ json_controller.cpp
@@ -1,6 +1,31 @@
 #include <ArduinoJson.h>
 #include "json_controller.h"
 
+
+/**
+ * returns the right Effect (json) based on a commandId. 
+ */
+char* buildEffect(int command) {
+  switch (command) {
+    case 1:
+      // Case: Lage horizontal.
+    case 2:
+      // Case: Lage vertikal gerade.
+    case 3:
+      // Case: Lage vertikal hoch. 
+    case 4:
+      // Case: Bewegung horizontal.
+    case 5:
+      // Case: Bewegung vertikal gerade.
+    case 6:
+      // Case: Bewegung vertikal hoch.
+    default:
+      // Dunkel ab wenn nichts bekannt ist.
+      return buildJson(0, 0, (int[3]){255, 0, 0}, nullptr, nullptr)
+  }
+}
+
+
 /**
  * Function to build the Json that controls the lights from the led-matrix.
  * - primary color is required, other colors are optional.
